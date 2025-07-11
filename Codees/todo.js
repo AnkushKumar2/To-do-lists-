b4dc8be5bd511a6797d9caeaec9f2d7b364fe566
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-07-11 22:05:39 +0530

    changes in js

--- Codees/todo.js
+++ Codees/todo.js
@@ -4,17 +4,24 @@ const toitems=document.getElementById('todoitems')
 
 addbttn.addEventListener('click',()=>{
     const value= toinput.value;
-    
-    const li=document.createElement('li')
-    li.innerText=value
-
-    const delbutton=document.createElement('button')
-    delbutton.innerText='X'
+    if(toinput.value===''){
+        alert("You must write something");
+    }
+    else{
+          const li=document.createElement('li')
+          li.innerText=value;
+           const delbutton=document.createElement('span')
+    delbutton.innerText="\u00d7"
     delbutton.addEventListener('click',()=>{
         li.remove()
     })
     li.appendChild(delbutton)
     toitems.appendChild(li)
+    }
+    
+    
+
+   
       toinput.value=''
  
 })
