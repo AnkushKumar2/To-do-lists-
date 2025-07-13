080f663c033393cf7c8610a5a1a00876825ae546
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-07-13 22:01:22 +0530

    added check button function in js

--- Codees/todo.js
+++ Codees/todo.js
@@ -25,4 +25,20 @@ addbttn.addEventListener('click',()=>{
       toinput.value=''
  
 })
+toitems.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+    }
+
+}, false);
+
+
+function saveData(){
+    localStorage.setItem("data",toitems.innerHTML);
+}
+
+
  
